95a746920cdf0de5046a5b4e04f84f1fc960cddb
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-16 14:16:35 +0530

    Created Java program for float addition using initialized variables

--- Java/program04.java
+++ Java/program04.java
@@ -1,14 +1,23 @@
-// for loop: used when we known fixed iterations to eg : count 0-9
+import java.util.Scanner;
 
-class Program37
+class Program04
 {
-    public static void main(String A[])
+    public static void main(String[] args)
     {
-        int iCnt = 0;
+        Scanner sobj = new Scanner(System.in);
 
-        for(iCnt =1 ; iCnt <= 10; iCnt ++)
-        {
-            System.out.println(iCnt);
-        }
+        float i = 0.0f;
+        float j = 0.0f;
+        float k = 0.0f;
+
+        System.out.println("Enter first number :");
+        i = sobj.nextFloat();
+
+        System.out.println("Enter second number :");
+        j = sobj.nextFloat();
+
+        k = i + j;
+
+        System.out.println("Addition is : " + k);
     }
 }
