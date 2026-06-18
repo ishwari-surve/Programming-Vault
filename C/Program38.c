ccf40f55dfbedabb89bd7b598573ab4d993c4372
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-18 17:12:41 +0530

    Print odd numbers 1 to N using step-by-2 increment

---
+++ C/Program38.c
@@ -0,0 +1,29 @@
+// differemt kind of for loop conditions
+//Input : 5
+//Output : 1 3 5
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt+=2) // 1 3 5
+    {
+        printf("%d\t", iCnt);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
