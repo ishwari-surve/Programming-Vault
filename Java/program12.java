15db71a2caf33dce4f2eab943dbb0496debc3971
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-19 23:38:30 +0530

    Refactor divisibility check into separate method

---
+++ Java/program12.java
@@ -0,0 +1,31 @@
+//Type 2
+
+import java.util.*;
+
+class program12
+{
+    void CheckDivisible(int iNo)
+    { 
+        if((iNo % 3 == 0) && (iNo % 5 == 0))
+        {
+            System.out.println("Number is Divisible by 3 and 5");
+        }
+        else
+        {
+            System.out.println("Number is Not Divisible by 3 and 5");
+        }
+    }
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the No: ");
+        iValue = sobj.nextInt();
+        
+        CheckDivisible(iValue);         // Error
+        
+        sobj.close();
+    }
+}
