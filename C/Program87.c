42cea8e3d4827473ee664a926ae9c7ddcf5b766f
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-20 23:43:42 +0530

    Demonstrate dynamic memory allocation with malloc and free

---
+++ C/Program87.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{   
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    
+    // Step 1: Aceept the Size of Array
+    printf("Enter Size of Array: \n");
+    scanf("%d",&iLength);
+
+    // Step 2: Allocate the Memory
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    // Step 3: Accept the Values from User
+    printf("Enter the Number: \n");
+    for(iCnt = 0 ; iCnt <iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    // Step 4: Used the Memory (Logic)
+    printf("The Numbers are: \n");
+    for(iCnt = 0 ; iCnt <iLength; iCnt++)
+    {
+        printf("%d\n",Brr[iCnt]);
+    }
+
+    // Step 5: Dellocate the memory
+    free(Brr);
+
+    return 0;
+}
