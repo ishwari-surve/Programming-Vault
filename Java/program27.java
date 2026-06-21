7764b476ab4f80eabe7edbe5bcb3ffd5b1b01bfd
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-21 23:31:30 +0530

    Combine flag and counter initialization inside for loop header

---
+++ Java/program27.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class NumberX
+{
+    public boolean CheckPrime(int iNo)
+    {
+        // Logic
+        int iCnt = 0; 
+        boolean bFlag = false;
+
+        for(bFlag = true,iCnt = 2; iCnt <= (iNo/2); iCnt++)
+        {
+            if ((iNo % iCnt == 0))
+            {
+               bFlag = false;
+               break;
+            }
+        }
+        
+        return bFlag;
+    }
+}
+
+class program27
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        boolean bRet = false;
+
+        System.out.println("Enter the No: ");
+        iValue = sobj.nextInt();
+        
+        NumberX nobj = new NumberX();
+
+        bRet = nobj.CheckPrime(iValue);
+
+        if (bRet == true)
+        {
+            System.out.println(iValue+" It is Prime Number");
+        }
+        else
+        {
+            System.out.println(iValue+" It is Not Prime Number");
+        }
+        sobj.close();
+    }
+}
+
+// Time Complexity: O(N/2)
+// Where N>=0   (where N is value which you have entered)
