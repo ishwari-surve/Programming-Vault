29ef1b0e481dc37995cf104e581d62d2989c80a6
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-21 23:48:13 +0530

    Combine odd and even digit count into single method

---
+++ Java/program37.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+class DigitX
+{
+    public void CountOddEvenDigits(int iNo)
+    {
+        int iDigit = 0;
+        int iCountOdd = 0;
+        int iCountEven = 0;
+        
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            if(iDigit % 2 != 0)
+            {
+                iCountOdd++;
+            }
+            else
+            {
+                iCountEven++;
+            }
+            iNo = iNo / 10;
+        }
+
+        System.out.println("Number of Even Digit: "+iCountEven);
+        System.out.println("Number of Odd Digit: "+iCountOdd);
+    }
+}
+
+class program37
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+        
+        int iValue = 0;
+        
+        System.out.println("Enter Number: ");
+        iValue = sobj.nextInt();
+
+        dobj.CountOddEvenDigits(iValue);
+
+    }
+}
