f6cb4dad66aa75bc94ed6207bb6ad5c8eee5bbf9
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-21 23:17:08 +0530

    Optimize factor search loop to check only up to N/2

---
+++ Java/program19.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class NumberX
+{
+    public void DisplayFactors(int iNo)
+    {
+        // Logic
+        int iCnt = 0;
+        for(iCnt = 1; iCnt <= (iNo/2); iCnt++)
+        {
+            if ((iNo % iCnt == 0))
+            {
+                System.out.println(iCnt);
+            }
+        } 
+    }
+}
+
+class program19
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the No: ");
+        iValue = sobj.nextInt();
+        System.out.println("Factors are: ");
+        
+        NumberX nobj = new NumberX();
+
+        nobj.DisplayFactors(iValue);
+
+        sobj.close();
+    }
+}
+
+// Time Complexity: O(N/2)
+// Where N>=0   
