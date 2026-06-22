e4ee5ef50cf2949d9ae926fa8a6c370479f3eca2
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-22 23:19:07 +0530

    Added program to print array elements using loop iteration

---
+++ Java/program45.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class program45
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+        
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.err.println(Arr[iCnt]);
+        }
+
+        sobj.close();
+
+    }
+}
