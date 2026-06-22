09e2b15add8aa14fbe6a3f9010196d2619f59360
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-22 23:18:17 +0530

    Added Java program to traverse and display array elements using for loop

---
+++ Java/program44.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program36
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+        int iCnt = 0;
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.err.println(Arr[iCnt]);
+        }
+
+        sobj.close();
+
+    }
+}
