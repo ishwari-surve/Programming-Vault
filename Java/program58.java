71013010960d3ca99e078518a9e119b5ac2d0aa3
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-22 23:33:39 +0530

    Added Java program to display star-number-star pattern using iterative loop

---
+++ Java/program58.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class program58
+{   
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.print("*"+iCnt+"*\t"); 
+        }
+        System.out.println();
+    }
+    
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the Number of Elements: ");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+    }
+}
