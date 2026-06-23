b34cd863ebb1e59531ca2958bed364ce41cc46f1
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-23 23:19:27 +0530

    Added program to display uppercase alphabets separated by '*' pattern with special handling for the last character

---
+++ Java/program67.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class program67
+{   
+    public static void Display(int iNo)
+    {   
+        int iCnt = 0;
+        char ch = '\0';
+
+        for(iCnt = 1, ch = 'A' ; iCnt <= iNo; iCnt++)
+        {   
+            if(iCnt == iNo)
+            {
+                System.out.println(ch);
+            }
+            else
+            {
+            System.out.print(ch+"\t*\t");
+            ch++;
+            }
+        }
+    }
+    
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the Number of Elements: ");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+    }
+}
