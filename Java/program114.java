7aef3705419733158293624ca9ceda8262d0ee31
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-02 22:13:38 +0530

    Add Java program demonstrating invalid string indexing using array notation

---
+++ Java/program114.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class program114
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Arr = null;
+
+        System.out.println("Enter String: ");
+        Arr = sobj.nextLine();
+
+        // Error
+        System.out.println(Arr[0]);
+        System.out.println(Arr[1]);
+        System.out.println(Arr[2]);
+
+        sobj.close();
+    }
+}
