e6d6f4cec2a9cddc89f96ed8fbd03102c69f9160
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-03 23:50:05 +0530

    Refactor string traversal to use toCharArray instead of charAt

---
+++ Java/program125.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class StringX
+{
+    public int CountCapital(String str)
+    {   
+        int i = 0, iCount = 0;
+        
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= 'A' && Arr[i] <= 'Z')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+
+    public int CountSmall(String str)
+    {   
+        int i = 0, iCount = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= 'a' && Arr[i] <= 'z')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+}
+
+class program125
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        StringX strobj = new StringX();
+
+        String data = null;
+        int iRet = 0;
+
+        System.out.println("Enter String: ");
+        data = sobj.nextLine();
+
+        iRet = strobj.CountCapital(data);
+        System.out.println("Number of Capital Characters are: "+iRet);
+
+        iRet = strobj.CountSmall(data);
+        System.out.println("Number of Small Characters are: "+iRet);
+
+    }
+}
