4ca2cbae408bccad62f1d50639399850e9ab2971
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-03 23:51:49 +0530

    Add method to count whitespace characters in string

---
+++ Java/program127.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+class StringX
+{
+    public int CountCapital(String str)
+    {   
+        int i = 0, iCount = 0;
+        
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= 'A' && Arr[i] <= 'Z')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+
+    public int CountSmall(String str)
+    {   
+        int i = 0, iCount = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= 'a' && Arr[i] <= 'z')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+
+    public int CountDigits(String str)
+    {   
+        int i = 0, iCount = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] >= '0' && Arr[i] <= '9')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+
+    public int CountSpace(String str)
+    {   
+        int i = 0, iCount = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] == ' ')
+            {
+                iCount++;
+            }
+        }
+        return iCount;
+    }
+}
+
+class program127
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        StringX strobj = new StringX();
+
+        String data = null;
+        int iRet = 0;
+
+        System.out.println("Enter String: ");
+        data = sobj.nextLine();
+
+        iRet = strobj.CountCapital(data);
+        System.out.println("Number of Capital Characters are: "+iRet);
+
+        iRet = strobj.CountSmall(data);
+        System.out.println("Number of Small Characters are: "+iRet);
+
+        iRet = strobj.CountDigits(data);
+        System.out.println("Number of Digits are: "+iRet);
+
+        iRet = strobj.CountSpace(data);
+        System.out.println("Number of white Spaces are: "+iRet);
+
+    sobj.close();
+    }
+}
