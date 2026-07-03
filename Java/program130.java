5bcc1fd33c18df0362dff154a604e37e8dd985c6
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-03 23:54:35 +0530

    Simplify Update method by returning new String directly without temp variable

---
+++ Java/program130.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class StringX
+{
+    public String Update(String str)
+    {
+        int i = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] == 'A' || Arr[i] == 'a')
+            {
+                Arr[i] = '_';
+            }
+        }
+        return new String(Arr);
+    }
+}
+
+class program130
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        StringX strobj = new StringX();
+        String sRet = null;
+        String data = null;
+
+        System.out.println("Enter String: ");
+        data = sobj.nextLine();
+
+        sRet = strobj.Update(data);
+        System.out.println("Updated String is: "+sRet);
+
+
+    sobj.close();
+    }
+}
