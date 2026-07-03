248aa7c7597c12c3a7aa982991cb4dde8c48c8a4
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-03 23:47:27 +0530

    Add Java program to accept and display string using Scanner nextLine

--- Java/program121.java
+++ Java/program121.java
@@ -1,1 +1,26 @@
-//113
+import java.util.*;
+
+class StringX
+{
+    public void Display(String str)
+    {
+        System.out.println("Received String is: "+str);
+    }
+}
+
+class program121
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        StringX strobj = new StringX();
+
+        String str = null;
+
+        System.out.println("Enter String: ");
+        str = sobj.nextLine();
+
+        strobj.Display(str);
+
+    }
+}
