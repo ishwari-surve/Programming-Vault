35f3e87224514bb0367317601817d389857b83a3
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-04 19:31:11 +0530

    Add Java program to convert lowercase character to uppercase using subtraction  - Declares char variable ch1 with value 'm' - Prints original lowercase character - Subtracts 32 from ASCII value of ch1 to get uppercase equivalent - Casts result back to char using (char) to display letter instead of number - Demonstrates reverse of previous program which added 32 for lowercase conversion

---
+++ Java/program138.java
@@ -0,0 +1,11 @@
+class program138
+{
+    public static void main(String A[])
+    {
+        char ch1 = 'm';
+
+        System.out.println(ch1);
+        System.out.println((char)(ch1-32));   
+
+    }
+}
