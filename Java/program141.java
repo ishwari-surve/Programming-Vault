97e79034cfd66c6ea24e51ae9cc1710d8c030373
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-05 20:52:11 +0530

    Add program141 with both toUpperX and toLowerX methods   Implemented toUpperX to convert lowercase letters to uppercase.   Implemented toLowerX to convert uppercase letters to lowercase.   Ensures digits, spaces, and special symbols remain unchanged.   Program reads input string and displays both uppercase and lowercase conversions.

--- Java/program141.java
+++ Java/program141.java
@@ -1,1 +1,57 @@
-//133
+import java.util.*;
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+        int i = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {   
+            if(Arr[i] >= 'a' && Arr[i] <= 'z')
+            {
+                Arr[i] = (char)(Arr[i] - 32);       
+            }
+        }
+        return new String(Arr);
+    }
+
+    public String toLowerX(String str)
+    {
+        int i = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {   
+            if(Arr[i] >= 'A' && Arr[i] <= 'Z')
+            {
+                Arr[i] = (char)(Arr[i] + 32);       
+            }
+        }
+        return new String(Arr);
+    }
+}
+
+class program141
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        StringX strobj = new StringX();
+        String sRet = null;
+        String data = null;
+
+        System.out.println("Enter String: ");
+        data = sobj.nextLine();
+
+        sRet = strobj.toUpperX(data);
+        System.out.println("Updated Upper String is: "+sRet);
+
+        sRet = strobj.toLowerX(data);
+        System.out.println("Updated Lower String is: "+sRet);
+
+
+    sobj.close();
+    }
+}
