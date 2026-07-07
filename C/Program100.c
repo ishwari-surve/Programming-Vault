07d6ee36d3dca15f7ff253f2c90bb5bc7a1e7d4f
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-07 21:39:14 +0530

    Add C program to find maximum element in array using dynamic memory allocation   Declared integer pointer Brr = NULL and accepted array size from user.   Allocated memory dynamically with malloc.   Accepted elements from user input.   Defined Maximum function that iterates through array and updates maximum value.   Returned maximum element to main and printed result.   Freed allocated memory to prevent leaks.

---
+++ C/Program100.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Time Complexity: O(n)
+
+int Maximum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMax = 0;
+
+    iMax = Arr[0];
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] > iMax)
+        {
+            iMax = Arr[iCnt];
+        }
+    }
+    return iMax;
+}
+
+int main()
+{   
+    int *Brr = NULL, iLength = 0, iCnt = 0,iRet = 0;
+    
+    printf("Enter the Size of Array: ");
+    scanf("%d",&iLength);
+    
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the Elements: ");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Maximum(Brr,iLength);
+
+    printf("Maximum Element is %d\n",iRet);
+
+    free(Brr);
+
+    return 0;
+}
