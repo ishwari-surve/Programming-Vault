08bac5b75bdaf7d377774894cc6958688ca25533
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-09 00:31:32 +0530

    Add C program to demonstrate pointer traversal in string   Defined str as pointer to string literal "Ganesh".   Used pointer dereferencing and increment operations to print characters sequentially.   Program outputs each character one by one until end of string.

--- C/Program109.c
+++ C/Program109.c
@@ -1,1 +1,27 @@
-//110
+#include<stdio.h>
+
+int main()
+{
+    char *str = "Ganesh";       // same as char str[] = "Ganesh";
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+    printf("%c\n",*str);
+    str++;
+
+
+    return 0;
+}
