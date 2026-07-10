73ae03c7cb4ce5a9044fad6a98809992334583d5
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-10 10:30:38 +0530

    Add C program to calculate string length using a pointer to constant characters

---
+++ C/Program122.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int strlenX(const char * str)
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    while(*str != '\0')
+    {
+        iCount++;
+        str++;      // str = str + 1;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter String:\n");
+    scanf("%[^'\n']s",Arr);           
+
+    iRet = strlenX(Arr);
+    
+    printf("String Length is: %d\n",iRet);
+
+    return 0;
+} 
