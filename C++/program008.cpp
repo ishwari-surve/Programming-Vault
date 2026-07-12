c22b82deac604a4221d776b3ee58ce4524f21884
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-12 23:43:25 +0530

    Add C++ program to demonstrate structure packing and class size using #pragma pack(1)

---
+++ C++/program008.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+};
+
+int main() 
+{   
+    ArrayX aobj;
+
+    cout<<sizeof(aobj)<<endl;   // 8 bytes
+
+    return 0;
+}
