a04f9654ab84dab5b41516762157ed8022b99be9
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-12 23:52:54 +0530

    Add C++ program to implement array summation using a class

---
+++ C++/program024.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    private:
+
+        int *Arr;
+        int iSize;
+
+        public: 
+            // Parameterized Constructor with default arguments
+            ArrayX(int X = 5)           // 15 will overwrite over 5
+            { 
+                iSize = X;                  
+                Arr = new int[iSize];       
+            }
+    
+            ~ArrayX()                       
+            {
+                delete []Arr;               
+            }
+
+            void Accept()
+            {   
+                int iCnt = 0;
+                cout<<"Enter the Elements: \n";
+                
+                for(iCnt = 0; iCnt < iSize; iCnt++)
+                {
+                    cin>>Arr[iCnt];
+                }
+            }
+
+             void Display()
+            {   
+                int iCnt = 0;
+                cout<<"Elements of the array are: \n";
+                
+                for(iCnt = 0; iCnt < iSize; iCnt++)
+                {
+                    cout<<Arr[iCnt]<<endl;
+                }
+            }
+
+            int Summation()
+            {
+                int iCnt = 0, iSum = 0;
+                for(iCnt = 0; iCnt < iSize; iCnt++)
+                {
+                    iSum = iSum + Arr[iCnt];
+                }
+                return iSum;
+            }
+};
+
+int main()
+{   
+    ArrayX *aobj = NULL;   
+    int iLength = 0, iRet = 0;
+
+    cout<<"Enter the Number of Elements: \n";
+    cin>>iLength;
+
+    aobj = new ArrayX(iLength);
+
+    aobj->Accept();
+    aobj->Display();
+
+    iRet = aobj->Summation();
+
+    cout<<"Summation is: "<<iRet<<endl;
+    delete aobj;
+    
+    return 0;
+}
