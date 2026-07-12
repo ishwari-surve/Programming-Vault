da38ab36f1b5d5500608cd5f609d5244205e6e11
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-12 23:47:25 +0530

    Add C++ program to demonstrate constructor and destructor invocation for dynamically allocated objects

---
+++ C++/program015.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        // parametrized constructor
+        ArrayX(int X)
+        {   
+            cout<<"Inside Constructor\n";
+            iSize = X;                  // Characterstics Initialisation
+            Arr = new int[iSize];       // Resource Allocation
+        }
+
+        // Destrcutor
+        ~ArrayX()                       
+        {
+            cout<<"Inside Destructor\n";
+            delete []Arr;               //  // Resource De-allocation 
+        }
+};
+
+int main()
+{   
+    // Static Memory allocation for object
+    // ArrayX aobj1(5);    
+
+    ArrayX *aobj1 = new ArrayX(5);
+
+    delete aobj1;
+
+    cout<<"End of main\n";
+
+    return 0;
+}
