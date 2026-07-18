97a2898e29b4d911d7ee38eb2d2c2842fa8d65d4
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-18 12:20:11 +0530

    Update UpdateLetter function to convert lowercase l to uppercase L in string  - Changed replacement character from hyphen to uppercase L - Converts every occurrence of lowercase l to its uppercase equivalent - Traverses string using non const char pointer with null terminator check - Modifies original array in place using pointer dereference assignment - Accepts string input with spaces using scanf custom format specifier - Displays updated string showing all lowercase l converted to uppercase L

---
+++ C/Program132.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void UpdateLetter(char * str)
+{
+    
+    while (*str != '\0')
+    {
+        if(*str == 'l')
+        {
+            *str = 'L';
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String:\n");
+    scanf("%[^'\n']s",Arr);           
+
+    UpdateLetter(Arr);
+    
+    printf("Updated String is: %s",Arr);
+
+    return 0;
+} 
