225fafbf4c8f00101f2d4c49810f882306e9a21f
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-20 11:47:04 +0530

    Add program to perform bitwise AND operation on two user input numbers

--- C/Program142.c
+++ C/Program142.c
@@ -1,1 +1,17 @@
-//143
+#include<stdio.h>
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0, Ans = 0;
+
+    printf("Enter First Number:\n ");
+    scanf("%d",&iNo1);
+
+    printf("Enter Second Number:\n ");
+    scanf("%d",&iNo2);
+
+    Ans = iNo1 & iNo2;
+    printf("Result is : %d",Ans);
+
+    return 0;
+}
