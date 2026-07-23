f38fa149da67f236097679d8c64d2fcb3016c47a
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-23 11:55:58 +0530

    Added C program to demonstrate bitwise NOT operator on an unsigned integer

--- C/Program175.c
+++ C/Program175.c
@@ -1,1 +1,17 @@
-//185
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+
+int main()
+{   
+    UINT iMask = 0xFFFFFFFF;
+    
+    printf("Before  : %X\n",iMask);
+
+    iMask = ~iMask;
+
+    printf("After   : %X\n",iMask);
+
+    return 0;
+}
