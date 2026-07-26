4612d0ad99edd183ed669340c7603b2800f44b5e
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-26 21:29:31 +0530

    Added C program to display array elements using iteration

---
+++ C/Program218.c
@@ -0,0 +1,21 @@
+//Recursion Problems on n Numbers
+#include<stdio.h>
+
+void Display(int Arr[], int iSize)
+{
+    int i = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        printf("%d\n",Arr[i]);
+    }
+}
+
+int main()
+{
+    int Brr[] = {10,20,30,40,50};
+
+    Display(Brr,5);
+
+    return 0;
+}
