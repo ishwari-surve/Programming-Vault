eb8812ce64146c3c3d7084aca3ac916661e58424
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-27 21:40:00 +0530

    Added C Program 224 - Accept and display a string

---
+++ C/Program224.c
@@ -0,0 +1,14 @@
+//Recursion Problems on strings
+#include<stdio.h>
+
+int main()
+{
+    char Arr[20] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    printf("%s\n",Arr);
+    
+    return 0;
+}
