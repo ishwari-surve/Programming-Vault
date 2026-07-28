430251ae35e15061a04e26e5fd26c4a87a76664e
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-28 18:12:06 +0530

    Added Python program to demonstrate dictionary data type

---
+++ Python/MappingX.py
@@ -0,0 +1,12 @@
+#dictionary
+
+Information={"Name":"Rahul","Age":25,"City":"Pune","Marks":89.90,"City": "Mumbai"}
+
+
+
+print(Information)
+
+print(Information["City"])
+Information["Age"]=26
+
+print(Information)
