be84a32c123befdca043c5d885859eb673b1008d
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-31 10:55:23 +0530

    Add files via upload

---
+++ Python/Seq.py
@@ -0,0 +1,14 @@
+#list Duplicate
+Value1=[10,20,30,40,10] 
+print(Value1[0]) #10
+Value1[2]=35
+print(Value1[2])
+
+
+#tuple Duplicate
+Value2=(10,20,30,40,10) 
+print(Value2[0]) #10
+
+#set No- Duplicate
+Value3={10,20,30,40,10} 
+print(Value3[0]) #error
