c279d1c9f4f412d3e55c9ae8ce9f3a6f493ce61a
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-31 10:48:01 +0530

    Add files via upload

---
+++ Python/IO.py
@@ -0,0 +1,9 @@
+print("Enter Your Name:")
+Name=input()
+
+print("Hello",Name)
+
+print("Enter your age:")
+Age=input()
+
+print("Your age is:",Age)
---
+++ Python/IOX.py
@@ -0,0 +1,9 @@
+Name=input("Enter Your Name:")
+
+
+print("Hello",Name)
+
+Age=input("Enter your age:")
+
+
+print("Your age is:",Age)
