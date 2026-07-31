9f3ff9983d52ac0ce4c0f5193115c1ed768aa686
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-31 10:47:14 +0530

    Added Python program to demonstrate memory usage using sys.getsizeof()

---
+++ Python/Size.py
@@ -0,0 +1,12 @@
+import sys
+
+No=1146574839298476489939920987478  #int
+
+Marks=90.90 #float
+
+Name="Rahul" #str
+
+print(sys.getsizeof(No))
+print(sys.getsizeof(Marks))
+print(sys.getsizeof(Name))
+
