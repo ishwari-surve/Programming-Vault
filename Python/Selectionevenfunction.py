a876d8763013480a191c70077d9a8bda4cb42a23
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-07 19:07:13 +0530

    Added Python program to demonstrate positional and keyword arguments while calling a user-defined function to check whether a number is even or odd

--- Python/Selectionevenfunction.py
+++ Python/Selectionevenfunction.py
@@ -2,7 +2,7 @@ def CheckEven(No):
     if(No % 2 == 0):
         print("It is Even")
     else:
-     print("It is odd")
+     print("It is odd") 
 
 def main():
     CheckEven(21)             #Positional
