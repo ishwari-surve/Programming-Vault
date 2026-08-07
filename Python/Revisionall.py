ce6fab5d06c4b37e20aa4c0fb546076afb9973b4
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-07 19:02:48 +0530

    Added Python program to demonstrate the Set and Dictionary data types, including duplicate handling in sets and key-value access in dictionaries

--- Python/Revisionall.py
+++ Python/Revisionall.py
@@ -7,6 +7,6 @@ print("---------------------------------------------")
 
 book={"Name":"Let us C","Price":350,"Author":"Y Kanetkar"}
 print(book)
-print(type(book))
+print(type(book)) 
 print(book["Price"])
 print("---------------------------------------------")
