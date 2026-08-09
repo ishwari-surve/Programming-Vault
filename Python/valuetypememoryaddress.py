a9cb501a149085205dcdefa453dfc9b9e1e76b18
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-09 19:42:23 +0530

    Add files via upload

---
+++ Python/valuetypememoryaddress.py
@@ -0,0 +1,13 @@
+# Assignment 2 - Question 1
+
+# Declare a variable
+x = 25
+
+# Display value
+print("Value:", x)
+
+# Display type
+print("Type:", type(x))
+
+# Display memory address
+print("Memory Address:", id(x))
