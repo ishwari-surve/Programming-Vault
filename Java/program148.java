ca73ebd6f84dc73ac7403960f6fb431521a24339
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-13 11:20:07 +0530

    Added Java program to demonstrate the trim() method by comparing String length before and after removing leading and trailing spaces

---
+++ Java/program148.java
@@ -0,0 +1,11 @@
+class program148
+{
+    public static void main(String A[]) 
+    {
+        String header = "    India  is  My  Country    ";
+        System.out.println("Before Trim: "+header.length());
+        header = header.trim();              
+
+        System.out.println("After Trim: "+header.length());
+    }
+}
