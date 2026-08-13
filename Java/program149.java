1a54c4c9fec0a80161646b42903989783b6ac646
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-13 11:20:53 +0530

    Added Java program to demonstrate the replaceAll() method and highlight the issue of not storing the modified String returned by the method

---
+++ Java/program149.java
@@ -0,0 +1,13 @@
+class program149
+{
+    public static void main(String A[])
+    {
+        String header = "marvellous";
+
+        System.out.println(header);
+
+        header.replaceAll("l", "L");    // Issue
+
+        System.out.println(header);
+    }
+}
