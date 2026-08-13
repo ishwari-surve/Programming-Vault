ad65d2db195211088a5b5e1dfa661fa31d6fa53f
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-13 11:18:41 +0530

    Added Java program to demonstrate the trim() method and highlight the issue of not storing the returned trimmed String

---
+++ Java/program146.java
@@ -0,0 +1,11 @@
+class program146
+{
+    public static void main(String A[]) 
+    {
+        String header = "    India is My Country    ";
+
+        header.trim();              // Issue
+
+        System.out.println(header.length());
+    }
+}
