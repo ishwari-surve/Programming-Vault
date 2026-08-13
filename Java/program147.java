cf44d32bd62b8e1ac052c8107245bfea331545bb
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-13 11:19:24 +0530

    Added Java program to demonstrate the trim() method by removing leading and trailing spaces from a String and storing the updated value

---
+++ Java/program147.java
@@ -0,0 +1,11 @@
+class program147
+{
+    public static void main(String A[]) 
+    {
+        String header = "    India is My Country    ";
+
+        header = header.trim();              // Issue Resolved
+
+        System.out.println(header.length());
+    }
+}
