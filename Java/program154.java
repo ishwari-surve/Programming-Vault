98a867c6fe3f56bbd05dfda2bab2856b22f58436
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-14 17:27:10 +0530

    Added Java program to accept a String from the user using Scanner and display the String along with its length

---
+++ Java/program154.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class program154
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String str = null;
+
+        System.out.println("Enter string : ");
+        str = sobj.nextLine();
+
+        System.out.println("String is : "+str+" having length : "+str.length());
+    }
+}
