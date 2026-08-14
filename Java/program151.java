595ae154d74e2d3e9b2d1c222883cbd13fe80b99
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-14 17:24:55 +0530

    Added Java program to demonstrate trim() and replaceAll() methods for removing extra spaces from a String

--- Java/program151.java
+++ Java/program151.java
@@ -1,1 +1,17 @@
-//143
+class program151
+{
+    public static void main(String A[])
+    {
+        String header = "    india  is  my  country    ";
+
+        System.out.println(header);
+
+        header = header.trim();
+
+        System.out.println(header);
+
+        header = header.replaceAll("  ", " ");
+
+        System.out.println(header);
+    }
+}
