05d8039016bd22e3f7d8e32ba5f4c1fc568ad3dc
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-19 12:20:14 +0530

    Added Program171 to pad the header with white spaces up to 100 characters

--- Java/program171.java
+++ Java/program171.java
@@ -1,1 +1,21 @@
-//163
+class program171
+{
+    public static void main(String A[])
+    {
+        String header = "Hello.txt 1078";
+
+        System.out.println("Actual header length : "+header.length());    // 8
+        
+        System.out.println("Number of white spaces we need : "+(100 - header.length()));    // 11
+        
+        int size = 100 - header.length();
+
+        for(int i = 1; i <= size; i++)
+        {
+            header = header + " ";
+        }
+    
+        System.out.println("Updated header length is : "+header.length());
+        System.out.println("Updated header is : "+header);
+    }
+}
