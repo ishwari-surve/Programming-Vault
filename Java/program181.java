8b18cb227ada0fd911cd398852691856c43e30c8
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-19 12:30:29 +0530

    Added Program181 to capitalize the first character and convert each word to Camel

---
+++ Java/program181.java
@@ -0,0 +1,49 @@
+/* 
+Camel Case
+
+Input : my name is ishwari
+Output : My Name is Ishwari
+
+Input : my NAME IS ishwari
+Output : My Name is Ishwari
+
+*/
+
+import java.util.*;
+
+class program181
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String str = sobj.nextLine();
+
+        str = str.trim();
+
+        str = str.replaceAll("\\s+"," ");
+
+        str = str.toLowerCase();
+
+        char Arr[] = str.toCharArray();
+        
+        if(Arr[0] >= 'a' && Arr[0] <= 'z')
+        {
+            Arr[0] = (char)(Arr[0] - 32);
+        }
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if(Arr[i] == ' ')
+            {
+                if(Arr[i + 1] >= 'a' && Arr[i + 1] <= 'z')
+                {
+                    Arr[i + 1] = (char)(Arr[i + 1] - 32);
+                }
+            }
+        }       
+        String output = new String(Arr);
+        System.out.println("Updated String is: "+output);
+    }
+}
