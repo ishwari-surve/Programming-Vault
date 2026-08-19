e6899b335a7d1bfc669041470d5c5ef6158098c3
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-19 12:34:20 +0530

    Added Program185 to reverse each word while preserving spaces between words

---
+++ Java/program185.java
@@ -0,0 +1,38 @@
+/*
+Input   : my name is amit
+Output  : ym eman si tima
+*/
+
+import java.util.*;
+
+class program185
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String str = sobj.nextLine();
+
+        str = str.trim();
+
+        str = str.replaceAll("\\s+", " ");
+
+        String Tokens[] = str.split(" ");
+        
+        StringBuffer sb = null;
+        StringBuffer FinalStr = new StringBuffer();
+
+        for(int i = 0; i < Tokens.length; i++)
+        {
+            sb = new StringBuffer(Tokens[i]);
+            sb = sb.reverse();
+            FinalStr = FinalStr.append(sb+" ");
+        }
+        String output = new String(FinalStr);
+
+        output = output.trim();
+
+        System.out.println(output);      
+    }
+}
