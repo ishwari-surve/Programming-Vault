b92243516d698077cd217901d4d23d36b034dd90
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-30 18:50:45 +0530

    Added Java Program to extract the last digits of a number using the modulus operator

---
+++ Java/program262.java
@@ -0,0 +1,12 @@
+class program262
+{
+    public static void main(String A[])
+    {
+        int iNo = 141376;
+
+        System.out.println(iNo % 10);
+        System.out.println(iNo % 100);
+        System.out.println(iNo % 1000);
+
+    }
+}
