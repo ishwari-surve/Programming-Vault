a159ad5e138c1a61cc4fa4d2cd402d15ae59cce8
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-07 17:28:00 +0530

    Added Java Program to create a Singly Linked List with node structure and initialize the first node reference and node count

---
+++ Java/program306.java
@@ -0,0 +1,32 @@
+
+class node
+{
+    public int data;
+    public node next;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+    }
+}
+
+class SinglyLL
+{
+    public node first;
+    public int iCount;
+
+    public SinglyLL()
+    {
+        this.first = null;
+        this.iCount = 0;
+    }
+}
+
+class program306
+{
+    public static void main(String A[])
+    {
+        SinglyLL sobj = new SinglyLL();
+    }
+}
