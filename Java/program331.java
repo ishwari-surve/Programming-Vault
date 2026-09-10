ac99a73e00ac928980d83fd29f0f3925e5373158
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-10 16:41:01 +0530

    Added Java Program to display all command line arguments using a for loop

---
+++ Java/program331.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class program331
+{
+    public static void main(String A[])
+    {
+        for(int i = 0; i < A.length; i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+}
