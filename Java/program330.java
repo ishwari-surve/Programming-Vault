e3a3adb7570b6f1787f7ae460d1297d46ad91fb2
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-10 16:40:12 +0530

    Added Java Program to display the number of command line arguments using length

---
+++ Java/program330.java
@@ -0,0 +1,11 @@
+//CommandLineinput
+
+import java.util.*;
+
+class program330
+{
+    public static void main(String A[])
+    {
+        System.out.println("Number of arguments are "+A.length);
+    }
+}
