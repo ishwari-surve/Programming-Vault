7e6c8128b002a02d1d9f69cd060151e7fcae97e9
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-11 10:46:56 +0530

    Added C Program to validate command line arguments and demonstrate addition using command line input

---
+++ C/program238.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main(int argc ,char *argv[])
+{
+   int Ans = 0;
+   if(argc != 3)
+   {
+     printf("Invalid number of arguments\n");
+     return -1;
+   }
+   Ans = argv[1] = argv[2]; //Error
+
+   printf("Addition id :%d",Ans);
+
+    return 0 ;
+}
