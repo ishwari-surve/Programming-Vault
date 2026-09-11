257b9a48ea376ceb4b68ceb8b631349158526c70
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-11 10:42:14 +0530

    Added Java Program to find the first non-repeating character using LinkedHashMap and Map.Entry

---
+++ Java/program338.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class program338
+{
+    public static void main(String A[])
+    {
+        
+       if(A.length != 1)
+        {
+            System.out.println("Invalid number of arguments");
+            return;
+        } 
+
+        String str = A[0];
+
+        LinkedHashMap <Character,Integer> frequency = new LinkedHashMap<Character,Integer>();
+
+        for(char ch : str.toCharArray())
+        {
+           frequency.put(ch,frequency.getOrDefault(ch,0)+1);
+        }
+        
+    
+
+        for(Map.Entry<Character,Integer> eobj : frequency.entrySet())
+        {
+            if(eobj.getValue() == 1)
+            {
+                System.out.println("First non repeatating character is :"+eobj.getKey());
+                break;
+            }
+        }
+
+      
+
+      
+    }
+}
