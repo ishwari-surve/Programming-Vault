ea9b7ee12d8b1f68fc4bef5200069ca1e5352a50
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-11 10:39:37 +0530

    Added Java Program to count character frequency using LinkedHashMap while preserving insertion order

---
+++ Java/program335.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class program335
+{
+    public static void main(String A[])
+    {
+        
+       if(A.length != 1)
+        {
+            System.out.println("Invalid number of arguments");
+            return;
+        } 
+
+        String str = A[0];
+
+        LinkedHashMap <Character,Integer> frequency = new LinkedHashMap<Character,Integer>();
+
+        for(char ch : str.toCharArray())
+        {
+           frequency.put(ch,frequency.getOrDefault(ch,0)+1);
+        }
+        
+        System.out.println(frequency);
+
+       
+    }
+}
