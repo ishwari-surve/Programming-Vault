b37d3089d551f8569aafce619246596eac95ebc0
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-12 16:53:10 +0530

    Added Java Program to sort student names alphabetically using ArrayList and Collections.sort()

---
+++ Java/program348.java
@@ -0,0 +1,31 @@
+// Q5 Student ranking system
+
+import java.util.*;
+
+
+ class program348
+{
+    public static void main(String A[])
+    {
+    
+       ArrayList <String> aobj = new ArrayList<String>();
+
+       aobj.add("Rahul");
+       aobj.add("Amit");
+       aobj.add("Pooja");
+       aobj.add("Neha");
+       aobj.add("Kiran");
+
+       System.out.println(aobj);
+
+       Collections.sort(aobj);
+
+       System.out.println(aobj);
+
+       
+       
+
+    }
+
+}
+
