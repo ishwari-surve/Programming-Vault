e888db0bcd9126ff2ae02cad658f0c1f49343de0
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-12 16:54:42 +0530

    Added Java Program to demonstrate String compareTo() method and lexicographical comparison

---
+++ Java/program350.java
@@ -0,0 +1,16 @@
+// Compareto concept
+
+import java.util.*;
+
+class program350
+{
+    public static void main(String A[])
+    {
+       System.out.println("Amit".compareTo("Amit"));
+       System.out.println("Amit".compareTo("Bhushan"));
+       System.out.println("Bhushan".compareTo("Amit"));
+
+    }
+
+}
+
