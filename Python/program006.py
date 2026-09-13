9f5b73bb0680c76b9350d4e2f0232264d52c8a66
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-13 20:46:29 +0530

    Added Python Program Perform addition using function

---
+++ Python/program006.py
@@ -0,0 +1,21 @@
+def AddTwoNumbers(fNo1, fNo2):
+    fAns = 0.0
+
+    fAns = fNo1 + fNo2
+
+    return fAns
+
+
+fValue1 = 0.0
+fValue2 = 0.0
+fResult = 0.0
+
+print("Enter first number :")
+fValue1 = float(input())
+
+print("Enter second number :")
+fValue2 = float(input())
+
+fResult = AddTwoNumbers(fValue1, fValue2)
+
+print("Additon is :", fResult)
