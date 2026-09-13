d404ea47c1f76090d25167a7051e8f2fe023e3b8
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-13 20:45:41 +0530

    Added Python Program Perform addition using float variables

---
+++ Python/program005.py
@@ -0,0 +1,13 @@
+fValue1 = 0.0
+fValue2 = 0.0
+fResult = 0.0
+
+print("Enter first number :")
+fValue1 = float(input())
+
+print("Enter second number :")
+fValue2 = float(input())
+
+fResult = fValue1 + fValue2
+
+print("Additon is :", fResult)
