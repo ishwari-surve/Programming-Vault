782d6a0e2b075da44f39611e55836c5de91d1fe9
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-14 23:05:02 +0530

    Added Python program to check even or odd number

---
+++ Python/program009.py
@@ -0,0 +1,12 @@
+iValue = 0
+iRemainder = 0
+
+print("Enter number :")
+iValue = int(input())
+
+iRemainder = iValue % 2
+
+if iRemainder == 0:
+    print("Number is Even :")
+else:
+    print("Number is odd :")
