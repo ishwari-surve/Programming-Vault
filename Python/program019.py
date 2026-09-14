c6fa83b37584e99537d7828f7ae649b6a9cd7bf0
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-14 23:10:16 +0530

    Added Python program with invalid age return value

---
+++ Python/program019.py
@@ -0,0 +1,23 @@
+def CalculateTicketPrice(iAge):
+    if iAge < 0:
+        return -1
+
+    if iAge >= 0 and iAge <= 5:
+        return 0
+    elif iAge >= 6 and iAge <= 18:
+        return 500
+    elif iAge >= 19 and iAge <= 50:
+        return 900
+    else:
+        return 400
+
+
+iValue = 0
+iRet = 0
+
+print("Please enter your age to calculate ticket price :")
+iValue = int(input())
+
+iRet = CalculateTicketPrice(iValue)
+
+print("Your ticket price will be", iRet, "ruppes")
