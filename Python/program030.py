49dd31cf95dcabcfdae99e5142e694a1b15c0ca7
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-15 22:17:43 +0530

    Added Python program to display numbers from 1 to given input

---
+++ Python/program030.py
@@ -0,0 +1,15 @@
+# For loop iterations
+
+def Display(iNo):
+
+    if iNo < 0:
+        print("INVALID INPUT")
+        return
+
+    for iCnt in range(1, iNo + 1):
+        print(iCnt)
+
+
+iValue = int(input("Enter the frequency: "))
+
+Display(iValue)
