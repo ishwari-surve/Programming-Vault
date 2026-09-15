27efd546f61aa2fe403749ac3b5ede391f3e1423
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-15 22:19:57 +0530

    Added Python program to accept and display user details

---
+++ Python/program034.py
@@ -0,0 +1,9 @@
+# Taking input from user
+
+sName = input("Enter your name: ")
+iAge = int(input("Enter your Age: "))
+fMarks = float(input("Enter your marks: "))
+
+print("Name :", sName)
+print("Age :", iAge)
+print("Marks :", fMarks)
