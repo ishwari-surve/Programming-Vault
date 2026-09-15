39202dc2b52f65787e6eadc901e4dbc98d87cfe2
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-15 22:21:11 +0530

    Added Python program to check divisibility by 3 and 5

---
+++ Python/program037.py
@@ -0,0 +1,8 @@
+# Check whether number is divisible by 3 and 5
+
+iValue = int(input("Enter number: "))
+
+if (iValue % 3 == 0) and (iValue % 5 == 0):
+    print("Number is dividble by 3 & 5")
+else:
+    print("Number is not dividible by 3 & 5")
