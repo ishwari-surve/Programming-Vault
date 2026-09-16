064c338a6e0f741744d30e34a73904c34cdfbb10
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-16 10:33:38 +0530

    Added Python Program to check divisibility by 3 and 5 with business logic outside the main function

---
+++ Python/program039.py
@@ -0,0 +1,8 @@
+def Checkdivisible(iNo):
+    if (iNo % 3 == 0) and (iNo % 5 == 0):
+        print("Number is divisible by 3 & 5")
+    else:
+        print("Number is not divisible by 3 & 5")
+
+
+Checkdivisible(int(input("Enter number : ")))
