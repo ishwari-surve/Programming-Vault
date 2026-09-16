0451f65633a2abc02a0863db8ac819d2b7a6ae22
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-16 10:40:38 +0530

    Added Python program to check perfect number

---
+++ Python/program048.py
@@ -0,0 +1,26 @@
+class NumberX:
+    def CheckPerfect(self, iNo):
+        iCnt = 0
+        iSum = 0
+
+        for iCnt in range(1, (iNo // 2) + 1):
+            if (iNo % iCnt) == 0:
+                iSum = iSum + iCnt
+
+        if iSum == iNo:
+            return True
+        else:
+            return False
+
+
+print("Enter number :")
+iValue = int(input())
+
+nobj = NumberX()
+
+bRet = nobj.CheckPerfect(iValue)
+
+if bRet == True:
+    print("It is perfect")
+else:
+    print("It is not perfect")
