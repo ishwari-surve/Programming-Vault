39fbde2f28dde6ce5c9a45a47994b30b4827a039
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-17 21:27:01 +0530

    Added Python program to display numbers from negative N to positive N

---
+++ Python/program061.py
@@ -0,0 +1,13 @@
+def Display(iNo):
+    iCnt = 0
+
+    for iCnt in range(-iNo, iNo + 1):
+        print(iCnt, end="\t")
+
+    print()
+
+
+print("Enter number : ")
+iValue = int(input())
+
+Display(iValue)
