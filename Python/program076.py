8749fab954205ddac841b33a36aba54e0e5597fc
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-19 22:10:21 +0530

    Added Python program to display positive digits using function

---
+++ Python/program076.py
@@ -0,0 +1,13 @@
+def DisplayDigit(iNo):
+    iDigit = 0
+
+    while iNo > 0:
+        iDigit = iNo % 10
+        print(iDigit)
+        iNo = iNo // 10
+
+
+print("Enter number :")
+iValue = int(input())
+
+DisplayDigit(iValue)
