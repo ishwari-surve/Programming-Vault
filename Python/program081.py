c869d056fe419f1fb142809c93186e0f58fc7a7c
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-20 22:38:55 +0530

    Added Python program to count digits of a number

---
+++ Python/program081.py
@@ -0,0 +1,21 @@
+class DigitX:
+    def CountDigits(self, iNo):
+        iDigit = 0
+        iCount = 0
+
+        while iNo != 0:
+            iDigit = iNo % 10
+            iCount = iCount + 1
+            iNo = iNo // 10
+
+        return iCount
+
+
+dobj = DigitX()
+
+print("Enter number : ")
+iValue = int(input())
+
+iRet = dobj.CountDigits(iValue)
+
+print("Number of digits are :", iRet)
