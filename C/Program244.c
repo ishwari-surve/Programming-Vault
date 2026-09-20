e1980b592dff512d3bf34309e1e8465425a673e1
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-20 22:51:28 +0530

    Added C program to open and close a file

---
+++ C/Program244.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<fcntl.h>      // Only Linux based 
+#include<unistd.h>  
+
+int main()
+{
+    int fd = 0;             // File Descriptior
+
+    fd = open("ABC.txt",O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to Open File\n");
+    }
+    else
+    {
+        printf("File Gets Succesfully Open with fd : %d\n",fd);
+        close(fd);
+    }
+    return 0;
+}
