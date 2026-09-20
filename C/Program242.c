afa97b5ac36194e9597ace5385f72baf12188a91
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-20 22:49:48 +0530

    Added C program to create a file and display file descriptor

---
+++ C/Program242.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+int main()
+{
+    int fd = 0;             // File Descriptior 
+
+    fd = creat("ABC.txt",0777);
+
+    if(fd == -1)
+    {
+        printf("Unable to Create File\n");
+    }
+    else
+    {
+        printf("File Gets Succesfully created with fd : %d\n",fd);
+    }
+
+    return 0;
+}
