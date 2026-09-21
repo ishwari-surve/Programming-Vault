d7aa084932a0e391104f6f42e798dc912ed886aa
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-21 18:30:15 +0530

    Added Java Program to store and display integer elements using ArrayList and enhanced for loop

---
+++ Java/program352.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class program419
+{
+    public static void main(String A[])
+    {
+        ArrayList<Integer> aobj = new ArrayList<Integer>();
+
+        aobj.add(10);
+        aobj.add(20);
+        aobj.add(30);
+        aobj.add(40);
+        aobj.add(50);
+
+        System.out.println("Elements are :");
+
+        for(Integer no : aobj)
+        {
+            System.out.println(no);
+        }
+    }
+}
