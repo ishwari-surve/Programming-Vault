cdf42ed4741ee64ca6dadbd6769440de35d87d5d
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-21 18:26:58 +0530

    Added Python program to accept and display array elements using loop

---
+++ Python/program099.py
@@ -0,0 +1,12 @@
+Arr = [0] * 5
+iCnt = 0
+
+print("Enter the elements : ")
+
+for iCnt in range(0, 5):
+    Arr[iCnt] = int(input())
+
+print("Elements of the array are : ")
+
+for iCnt in range(0, 5):
+    print(Arr[iCnt])
