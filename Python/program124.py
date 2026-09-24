726f0bd0150972a33057ac21a06c821267ffb413
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-24 17:04:22 +0530

    Added Python program to accept dynamic array elements

---
+++ Python/program124.py
@@ -0,0 +1,12 @@
+iLength = 0
+iCnt = 0
+
+print("Enter number of elements : ")
+iLength = int(input())
+
+Brr = [0] * iLength
+
+print("Enter the elements : ")
+
+for iCnt in range(0, iLength):
+    Brr[iCnt] = int(input())
