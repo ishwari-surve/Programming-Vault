92f337d56804d242f3275ddd77403271435d433b
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-25 11:55:02 +0530

    Implemented unique subject management using HashSet

---
+++ Java/program360.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program360
+{
+    public static void main(String A[])
+    {
+        HashSet<String> hobj = new HashSet<String>();
+
+        hobj.add("C");
+        hobj.add("C++");
+        hobj.add("Java");
+        hobj.add("Java");
+
+        System.out.println(hobj);
+    }
+}
