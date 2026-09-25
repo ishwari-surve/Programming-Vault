4761bfe4da05d4fd3e9e548f76097d0312e23cfd
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-25 11:47:45 +0530

    Implemented key value storage using HashMap

---
+++ Java/program358.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program425
+{
+    public static void main(String A[])
+    {
+        HashMap<Integer,String> hobj =
+            new HashMap<Integer,String>();
+
+        hobj.put(1,"Amit");
+        hobj.put(2,"Pooja");
+        hobj.put(3,"Rahul");
+
+        System.out.println(hobj);
+    }
+}
