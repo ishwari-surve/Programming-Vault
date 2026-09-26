4841ab30726f8c29266c74353c61931b31eeede5
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-26 12:25:40 +0530

    Implemented array to ArrayList conversion

---
+++ Java/program370.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class program437
+{
+    public static void main(String A[])
+    {
+        String Arr[] = {"C","C++","Java"};
+
+        ArrayList<String> list =
+            new ArrayList<String>(Arrays.asList(Arr));
+
+        System.out.println(list);
+    }
+}
