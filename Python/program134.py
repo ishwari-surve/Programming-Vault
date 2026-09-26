42969e63339078e5694a8338109f56aae635192c
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-26 12:57:16 +0530

    Added Python program to perform linear search for user input

---
+++ Python/program134.py
@@ -0,0 +1,30 @@
+def LinearSearch(Arr,iSize,iNo):
+    iCnt = 0
+    bFlag = False
+
+    for iCnt in range(0,iSize):
+        if Arr[iCnt] == iNo:
+            bFlag = True
+            break
+
+    return bFlag
+
+print("Enter the number of elements : ")
+iLength = int(input())
+
+Brr = [0] * iLength
+
+print("Enter the elements : ")
+
+for iCnt in range(0,iLength):
+    Brr[iCnt] = int(input())
+
+print("Enter the element that you want to search : ")
+iValue = int(input())
+
+bRet = LinearSearch(Brr,iLength,iValue)
+
+if bRet == True:
+    print("Element is present")
+else:
+    print("Element is not present")
